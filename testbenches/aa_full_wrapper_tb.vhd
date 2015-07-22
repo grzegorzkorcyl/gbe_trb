288b60c71ae4f31e63ecb2f5e75a1245181509ae
gk <grzegorz.korcyl@gmail.com>
2015-07-22 12:02:04 +0200

    update

--- testbenches/aa_full_wrapper_tb.vhd
+++ testbenches/aa_full_wrapper_tb.vhd
@@ -40,7 +40,7 @@ begin
 			LINK_HAS_PING             => "1111",
 			LINK_HAS_ARP              => "1111",
 			LINK_HAS_DHCP             => "1111",
-			LINK_HAS_READOUT          => "1000",
+			LINK_HAS_READOUT          => "0101",
 			LINK_HAS_SLOWCTRL         => "1000",
 			NUMBER_OF_OUTPUT_LINKS    => 4
 		)
