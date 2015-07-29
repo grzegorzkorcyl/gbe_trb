3922717b7b865f5a2f6a169eba03516e308d285d
gk <grzegorz.korcyl@gmail.com>
2015-07-29 18:06:49 +0200

    update

--- base/trb_net16_gbe_ipu_interface.vhd
+++ base/trb_net16_gbe_ipu_interface.vhd
@@ -96,7 +96,7 @@ architecture RTL of trb_net16_gbe_ipu_interface is
 	signal bank_select                                                          : std_logic_vector(3 downto 0);
 	signal readout_ctr                                                          : std_logic_vector(23 downto 0) := x"000000";
 	signal pc_ready_q                                                           : std_logic;
-	signal sf_afull_q,sf_afull_qq, sf_afull_qqq, sf_afull_qqqq					: std_logic;
+	signal sf_afull_q,sf_afull_qq, sf_afull_qqq, sf_afull_qqqq, sf_afull_qqqqq  : std_logic;
 	signal sf_aempty                                                            : std_logic;
 	signal rec_state, load_state                                                : std_logic_vector(3 downto 0);
 	signal queue_size                                                           : std_logic_vector(17 downto 0);
@@ -210,8 +210,10 @@ begin
 			sf_afull_qq <= sf_afull_q;
 			sf_afull_qqq <= sf_afull_qq;
 			sf_afull_qqqq <= sf_afull_qqq;
+			sf_afull_qqqqq <= sf_afull_qqqq;
 
-			if (sf_afull_q = '0' and save_current_state = SAVE_DATA and FEE_DATAREADY_IN = '1' and FEE_BUSY_IN = '1') then
+			--if (sf_afull_q = '0' and save_current_state = SAVE_DATA and FEE_DATAREADY_IN = '1' and FEE_BUSY_IN = '1') then
+			if (sf_afull_qqqqq = '0' and save_current_state = SAVE_DATA and FEE_DATAREADY_IN = '1' and FEE_BUSY_IN = '1') then
 				sf_wr_en <= '1';
 			elsif (save_current_state = SAVE_EVT_ADDR) then
 				sf_wr_en <= '1';
