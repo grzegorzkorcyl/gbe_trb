95839ad9dbdf3fe1ccc9979093dde06cdf1b7b33
gk <grzegorz.korcyl@gmail.com>
2015-08-05 13:36:50 +0200

    update

--- base/trb_net16_gbe_event_constr.vhd
+++ base/trb_net16_gbe_event_constr.vhd
@@ -196,41 +196,28 @@ end process DF_QQ_PROC;
 	end generate ready_impl_gen;
 	
 	ready_sim_gen : if DO_SIMULATION = 1 generate
---		READY_PROC : process
---		begin
---			PC_READY_OUT <= '1';
---			
---			wait for 2700 ns;
---			wait until rising_edge(CLK);
---			PC_READY_OUT <= '0';
---			wait until rising_edge(CLK);
---			PC_READY_OUT <= '1';
---			
---			wait for 300 ns;
---			wait until rising_edge(CLK);
---			PC_READY_OUT <= '0';
---			wait until rising_edge(CLK);
---			wait until rising_edge(CLK);
---			PC_READY_OUT <= '1';
---			
---			wait for 300 ns;
---			wait until rising_edge(CLK);
---			PC_READY_OUT <= '0';
---			wait until rising_edge(CLK);
---			wait until rising_edge(CLK);
---			wait until rising_edge(CLK);
---			PC_READY_OUT <= '1';
---			
---			
---			wait;
---		end process READY_PROC;
-
-		READY_PROC : process(CLK)
+		READY_PROC : process
 		begin
-			if rising_edge(CLK) then
-				PC_READY_OUT <= not df_full;
-			end if;	
+			PC_READY_OUT <= '1';
+			
+			wait for 22000 ns;
+			wait until rising_edge(CLK);
+			PC_READY_OUT <= '0';
+			wait until rising_edge(CLK);
+			wait until rising_edge(CLK);
+			wait until rising_edge(CLK);
+			PC_READY_OUT <= '1';
+			
+			
+			wait;
 		end process READY_PROC;
+
+--		READY_PROC : process(CLK)
+--		begin
+--			if rising_edge(CLK) then
+--				PC_READY_OUT <= not df_full;
+--			end if;	
+--		end process READY_PROC;
 
 	end generate ready_sim_gen;
 
