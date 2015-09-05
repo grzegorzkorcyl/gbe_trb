3d3046c86495b26b15f15e3a9885e13dd2b4f224
gk <grzegorz.korcyl@gmail.com>
2015-09-05 22:19:08 +0200

    update

--- base/gbe_ipu_dummy.vhd
+++ base/gbe_ipu_dummy.vhd
@@ -302,7 +302,7 @@ begin
 				end if;
 
 			when LOOP_OVER_DATA =>
-				if (to_integer(unsigned(data_ctr)) = (2 * (to_integer(unsigned(test_data_len)) - 1))) then
+				if (to_integer(unsigned(data_ctr)) = (2 * (to_integer(unsigned(test_data_len)) - 1)) and FEE_READ_IN = '1') then
 					next_state <= WAIT_A_SEC_7;
 				else
 					next_state <= LOOP_OVER_DATA; --SEND_ONE_WORD;
