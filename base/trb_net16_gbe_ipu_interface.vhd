a156baed4bc4296a47524629fb154d33e9989d73
gk <grzegorz.korcyl@gmail.com>
2015-10-13 11:55:37 +0200

    update

--- base/trb_net16_gbe_ipu_interface.vhd
+++ base/trb_net16_gbe_ipu_interface.vhd
@@ -847,7 +847,7 @@ begin
 				sf_rd_en <= '1';
 			else
 				if (PC_READY_IN = '1') then
-					if (load_current_state = LOAD and sf_eos_qq = '0') then
+					if (load_current_state = LOAD) then
 						sf_rd_en <= '1';
 					--elsif (load_current_state = CLOSE_SUB and last_three_bytes /= x"0") then
 					--	sf_rd_en <= '1';
